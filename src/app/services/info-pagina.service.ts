76de48509beb05623c8041a064d86d04fca91662
Luis Martinez <lmartinezocoro@gmail.com>
2019-02-12 21:48:40 -0500

    Se cargan datos de archivo JSON

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  constructor( private http: HttpClient) { 
+
+  	// Leer el archivo JSON
+  	this.http.get('assets/data/data-pagina.json')
+  	.subscribe( resp =>{
+  		console.log(resp);
+  		
+  	});
+
+
+  }
+}
